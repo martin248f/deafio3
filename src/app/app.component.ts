6bc92f1d08c717eab6c52921007ce2a67324cd80
martin <martin248f@gmail.com>
2021-12-02 00:00:30 -0300

    commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Validators, FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,20 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'desafio3';
+
+  form:FormGroup;
+  estadoFormulario:any;
+
+  constructor(private fb:FormBuilder) {
+    this.form = this.fb.group({
+      email:['', [Validators.required, Validators.email]],
+      password:['', [Validators.required, Validators.minLength(6)]],
+      terminos:['', [Validators.required, Validators.requiredTrue]],
+    });
+  }
+  
+  send():any {
+    console.log(this.form.value)
+  }
+  
 }
